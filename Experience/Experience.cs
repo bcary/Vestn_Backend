e3e578197431128964e1b724e890e40e33b78900
Brian Cary <briancary@vestn.com/n>
2012-06-23 16:06:57 -0500

    I have local changes that have not been committed on this branch, that were planned to be the first commits on my next branch. To make things easier, and so that I don't have to stash any changes, I will commit these to this branch so that it can be merged.

---
+++ Experience/Experience.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Experience
+{
+    public class Experience
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string company { get; set; }
+    }
+}
